68e605c3a9f83a361c3735d8b7b438ffc9d146d5
cs04905 <115148415+cs04905@users.noreply.github.com>
2024-10-22 10:32:45 +0300

    Completed lab01

--- lab02.s
+++ lab02.s
@@ -10,8 +10,36 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+  add t0 , zero , a1 #load size to t0
+     slli t1, t0, 2    #offset
+     add t2, a0, t1  #address of last element
+     lw s1, 0(t2)
+
+     addi t3, a0, -4  #out of bounds index for beq check
+    
+     add s0, zero, zero #init
+    
+     beq a1, zero, exit_not_found #check for empty array
+loop: 
+    beq t2, t3, exit_not_found
+    bne a2, s1, else
+    j exit_found
+
+
+
+else:
+    addi t2, t2, -4
+    lw s1, 0(t2)
+    j loop
+    
+exit_found:
+    add s0, t2, zero
+    j done
+    
+exit_not_found:
+    add s0, zero, zero
+    j done
+
 #-----------------------------
 done:
     addi a7, zero, 10 
